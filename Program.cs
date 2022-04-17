86f072f0736e05286c0f8ce353149efaca6da7d3
Samir Chabou <samirchabou2@gmail.com>
2022-04-17 16:11:35 +0100

    commit-f1v8

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Application_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!_f1v7");
+            Console.WriteLine("Hello World!_f1v8");
         }
     }
 }
